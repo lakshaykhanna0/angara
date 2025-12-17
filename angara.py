9e81e2a7e47c88de1eb1de263875f6893a7264fe
lakshaykhanna1 <lakshaykhanna@adventus.io>
2025-12-17 12:01:17 +0530

    Initial commit

---
+++ angara.py
@@ -0,0 +1,75 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+CHROME_DRIVER_PATH = "/usr/bin/chromedriver"
+BASE_URL = "https://www.saucedemo.com"
+
+
+class TestSwagLabsCheckout(unittest.TestCase):
+
+    def setUp(self):
+        service = Service(CHROME_DRIVER_PATH)
+        options = webdriver.ChromeOptions()
+        options.add_argument("--start-maximized")
+
+        self.driver = webdriver.Chrome(service=service, options=options)
+        self.driver.get(BASE_URL)
+        self.wait = WebDriverWait(self.driver, 10)
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_01_login_success(self):
+        self.driver.find_element(By.ID, "user-name").send_keys("standard_user")
+        self.driver.find_element(By.ID, "password").send_keys("secret_sauce")
+        self.driver.find_element(By.ID, "login-button").click()
+
+        title = self.wait.until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "title"))
+        )
+
+        self.assertEqual(title.text, "Products")
+        self.assertIn("inventory.html", self.driver.current_url)
+
+    def test_02_login_failure(self):
+        self.driver.find_element(By.ID, "user-name").send_keys("locked_out_user")
+        self.driver.find_element(By.ID, "password").send_keys("secret_sauce")
+        self.driver.find_element(By.ID, "login-button").click()
+
+        error = self.wait.until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "[data-test='error']"))
+        )
+
+        self.assertEqual(
+            error.text,
+            "Epic sadface: Sorry, this user has been locked out."
+        )
+
+    def test_03_add_to_cart_flow(self):
+        self.driver.find_element(By.ID, "user-name").send_keys("standard_user")
+        self.driver.find_element(By.ID, "password").send_keys("secret_sauce")
+        self.driver.find_element(By.ID, "login-button").click()
+
+        add_to_cart = self.wait.until(
+            EC.element_to_be_clickable((By.ID, "add-to-cart-sauce-labs-backpack"))
+        )
+        add_to_cart.click()
+
+        badge = self.wait.until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "shopping_cart_badge"))
+        )
+        self.assertEqual(badge.text, "1")
+
+        self.driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
+        item = self.wait.until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "inventory_item_name"))
+        )
+        self.assertEqual(item.text, "Sauce Labs Backpack")
+
+
+if __name__ == "__main__":
+    unittest.main()
